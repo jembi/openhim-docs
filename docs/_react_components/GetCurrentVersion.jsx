fc74564a286bc03261e9fe2c8cbc108692e5d290
Martin Brocker <martin.brocker@jembi.org>
2019-12-13 16:22:38 +0200

    Remove commented out code
    OHM-934

--- docs/_react_components/GetCurrentVersion.jsx
+++ docs/_react_components/GetCurrentVersion.jsx
@@ -1,21 +1,14 @@
 import React, { Component } from 'react'
 
-// import versions from '../../versions.json'
-
 class GetCurrentVersion extends Component {
   constructor(props) {
     super(props)
 
-    // console.log(versions)
-    console.log(window.location.href)
-
-    // const regex = /\/docs\/(.*?)\// // search for value between "/docs/" and the following "/" to get the version
     const regex = /[\d].[\d].x/ // search for value between "/docs/" and the following "/" to get the version
     let extractedVersion = window.location.href.match(regex)
 
     let actualVersion = 'master'
     if (extractedVersion) {
-      console.log(extractedVersion)
       actualVersion = `v${extractedVersion[0].replace('x', '0')}`
     }
 
